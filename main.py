ffe1d3a9b31ead23ce80a0412adef4fd4155a497
Winter <85322414+winterhascoming@users.noreply.github.com>
2021-09-09 20:59:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import random
 
-def main ():
+lista = [1,2,3,4,5]
+portal = random.choice(lista)
+sala = 1
+
+def myApp():
     tentativas = 1
     sala = 1
     vermelho = 1
     preto = 2
-    lista = [1,2,3,4,5]
-    portal = random.choice(lista)
 
     while (tentativas <=7 ):
         escolhido = float(input("Qual será o caminho escolhido? \n"))
         if escolhido == 1:
             sala = sala + vermelho
             print("Você está na sala:", sala)
-        elif sala == 8:
-            sala = sala + portal
-            print("Você está na sala:", sala)
+        elif sala == 8: #só esta funcionando quando o caminho escolhido estiver no else
+            print("Você entrou pelo portal e sua nova sala sera sorteada")
+            sala = portal
+            print("Sua nova sala é:", sala)
         else:
             sala = sala + preto
             print("Você está na sala:", sala)
         tentativas = tentativas + 1
         print("Essa é sua", tentativas, "ª tentativa")
-main()
+
+myApp()
+
+def main():
+    myApp
+
+if __name__ == "__main__":
+    main()
