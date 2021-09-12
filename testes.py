3e1627a41c8bb210018735a845f09ed4a9f8060a
fenanda <52468075+fenanda@users.noreply.github.com>
2021-09-12 17:15:53 -0300

    Código Base
    Esse código foi criado como um teste para o desenvolvimento da estrutura de repetição principal. Foi testado nele como seria feito a interação com o usuário para a escolha dos caminhos e a exibição de sua posição.

--- testes.py
+++ testes.py
@@ -1,24 +1,15 @@
-# falta finalizar o código => protótipo
-#caminho preto +2 e vermelho +1
-
 from random import randint
-
 sala = 1
-tentativa = 1
-
-while (tentativa <= 7 and sala <=8):
-    caminho = float(input("digite o caminho"))
-    caminho 
-    if (caminho == 1):
+tentativa=1 
+caminho = float(input("digite o caminho escolhido"))
+if(caminho == 1):
+        print("Você escolheu o caminho preto")
         sala = sala + 2
         print("vc esta na sala", sala)
-    elif(sala == 8):
+elif(sala == 8):
         sala = randint(1,5)
         print("sala do portal", sala)
-    else:
+else:
+        print("Você escolheu o caminho preto")
         sala = sala + 1
         print("vc está na sala", sala)
-    tentativa = tentativa + 1
-
-if(sala == 9 and tentativa <= 7):
-    print("ganhou")
